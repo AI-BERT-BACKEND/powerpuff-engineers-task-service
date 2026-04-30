10fca2303bdf81ffa0d6a715d1f0f437c77affa6
Daniel Felipe Rayo Rodriguez <daniel.rayo@mail.escuelaing.edu.co>
2026-04-29 21:06:45 -0500

    :cat: añado el aplication.yml junto con la pipeline CI

---
+++ src/test/java/com/aibert/dosw/TaskApplicationTest.java
@@ -0,0 +1,13 @@
+package com.aibert.dosw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
